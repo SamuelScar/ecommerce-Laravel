26f2d84c50bcad852eae69a58d32b1c583c48172
Samuel Almeida <souzacarvalhosamuel@gmail.com>
2022-11-22 07:29:03 -0300

    - Adicionado rotas do admin - Criado view de adicionar produto - Criado metodos  de edição de criação de produtos

--- app/Http/Controllers/AdminProductController.php
+++ app/Http/Controllers/AdminProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class AdminProductController extends Controller
@@ -9,14 +10,36 @@ class AdminProductController extends Controller
     
     public function index(){
 
-        return view("admin.products");
+        $products = Product::all();
+        return view("admin.products", compact('products') );
 
     }
 
+    // Mostar página de edição
     public function edit(){
 
         return view("admin.products_edit");
 
     }
 
+    // Recebe requisição para dar update
+    public function update(){
+
+        
+
+    }
+
+    // Mostrar página de criar
+    public function create(){
+
+        return view('admin.product_create');
+        
+    }
+
+    // Recebe a requisição de criar
+    public function store(){
+
+
+        
+    }
 }

b91f99ecbf6a4fe1fb62b46dd6bff466a0880cb6
Samuel Almeida <souzacarvalhosamuel@gmail.com>
2022-11-24 07:16:26 -0300

    - Adicionado validação do fomulario de criação de produtos - Correção de view de nomeclatura dos produtos

--- app/Http/Controllers/AdminProductController.php
+++ app/Http/Controllers/AdminProductController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Str;
+use Symfony\Component\Console\Input\Input;
 
 class AdminProductController extends Controller
 {
@@ -37,9 +40,28 @@ class AdminProductController extends Controller
     }
 
     // Recebe a requisição de criar
-    public function store(){
+    public function store(Request $request){
 
+        $input = $request->validate([
+            "name" => "string|required",
+            "price" => "string|required",
+            "stock" => "integer|nullable",
+            "cover" => "file|nullable",
+            "description" => "string|nullable"
+        ]);
 
+        $input["slug"] = Str::slug($input["name"]);
+
+        // Verifica o arquivo e pega o diretorio real onde estará armazenado
+        if( !empty($input["cover"]) && $input["cover"]->isValid() ){
+            $file = $input["cover"];
+            $path = $file->store("products");
+            $input["cover"] = $path;
+        }
+
+        Product::create($input);
+        
+        return Redirect::route("admin.product");
         
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/product/{product:slug}', [ProductController::class, 'show'])->name(
 // Admin routes
 Route::get('/admin/products', [AdminProductController::class, 'index'])->name("admin.product");
 Route::get('/admin/products/create', [AdminProductController::class, 'create'])->name("admin.product.create");
-Route::post('/admin/products/create', [AdminProductController::class, 'create'])->name("admin.product.create");
+Route::post('/admin/products', [AdminProductController::class, 'store'])->name("admin.product.store");
 
 Route::get('/admin/products/{product}/edit', [AdminProductController::class, 'edit'])->name("admin.product.edit");
 Route::put('/admin/products/{product}', [AdminProductController::class, 'update'])->name("admin.product.update");

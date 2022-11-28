68cf83dd49cf1a422a50eec373e81e551d4ec37e
Samuel Almeida <souzacarvalhosamuel@gmail.com>
2022-11-28 07:28:10 -0300

    - Adicionado função de update do produto - Ajustado renderização das imagens

--- app/Http/Controllers/AdminProductController.php
+++ app/Http/Controllers/AdminProductController.php
@@ -19,17 +19,36 @@ class AdminProductController extends Controller
     }
 
     // Mostar página de edição
-    public function edit(){
+    public function edit(Product $product){
 
-        return view("admin.products_edit");
+        return view("admin.products_edit", [
+            "product" => $product
+        ]);
 
     }
 
     // Recebe requisição para dar update
-    public function update(){
+    public function update(Product $product, Request $request){
 
-        
+        $input = $request->validate([
+            "name" => "string|required",
+            "price" => "string|required",
+            "stock" => "integer|nullable",
+            "cover" => "file|nullable",
+            "description" => "string|nullable"
+        ]);
 
+         // Verifica o arquivo e pega o diretorio real onde estará armazenado
+        if( !empty($input["cover"]) && $input["cover"]->isValid() ){
+            $file = $input["cover"];
+            $path = $file->store("products");
+            $input["cover"] = $path;
+        }
+
+        $product->fill($input);
+        $product->save();
+
+        return Redirect::route("admin.product");
     }
 
     // Mostrar página de criar

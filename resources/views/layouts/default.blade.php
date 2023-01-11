26706d72fbad7974d079d1526b886e389030c564
Samuel Souza <souzacarvalhosamuel@gmail.com>
2023-01-11 20:12:50 -0300

    - Retirado CDN do Tailwind CSS e instalado o mesmo no projto

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -10,8 +10,8 @@
         http-equiv="X-UA-Compatible"
         content="ie=edge"
     >
+    @vite('resources/css/app.css')
     <title>Document</title>
-    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
     
@@ -27,7 +27,8 @@
                 <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
                     <a href="/" class="mr-5 hover:text-gray-900">Home</a>
                 </nav>
-                <a href="{{ route("admin.product") }}" class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base md:mt-0">Admin
+                <a href="{{ route("admin.product") }}" class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base md:mt-0">
+                    Admin
                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
                         <path d="M5 12h14M12 5l7 7-7 7"></path>
                     </svg>

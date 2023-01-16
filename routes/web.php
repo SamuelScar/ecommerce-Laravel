4ad98b8b131b80bbe096c0044113106b01ce5ae8
Samuel Souza <souzacarvalhosamuel@gmail.com>
2023-01-16 07:31:32 -0300

    - Adicionado Laravel Breeze ao projeto

--- routes/web.php
+++ routes/web.php
@@ -14,19 +14,36 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
 */
 
 Route::get('/', [HomeController::class, 'index'])->name("home");
 Route::get('/product/{product:slug}', [ProductController::class, 'show'])->name("product");
 
-// Admin routes
-Route::get('/admin/products', [AdminProductController::class, 'index'])->name("admin.product");
-Route::get('/admin/products/create', [AdminProductController::class, 'create'])->name("admin.product.create");
-Route::post('/admin/products', [AdminProductController::class, 'store'])->name("admin.product.store");
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/admin/products/{product}/edit', [AdminProductController::class, 'edit'])->name("admin.product.edit");
-Route::put('/admin/products/{product}', [AdminProductController::class, 'update'])->name("admin.product.update");
+Route::middleware('auth')->group(function () {
 
-Route::get('/admin/products/{product}/delete', [AdminProductController::class, 'destroy'])->name("admin.product.destroy");
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-Route::get('/admin/products/{product}/delete-image', [AdminProductController::class, 'destroyImage'])->name("admin.product.destroyImage");
+    Route::get('/admin/products', [AdminProductController::class, 'index'])->name("admin.product");
+    Route::get('/admin/products/create', [AdminProductController::class, 'create'])->name("admin.product.create");
+    Route::post('/admin/products', [AdminProductController::class, 'store'])->name("admin.product.store");
+    Route::get('/admin/products/{product}/edit', [AdminProductController::class, 'edit'])->name("admin.product.edit");
+    Route::put('/admin/products/{product}', [AdminProductController::class, 'update'])->name("admin.product.update");
+    Route::get('/admin/products/{product}/delete', [AdminProductController::class, 'destroy'])->name("admin.product.destroy");
+    Route::get('/admin/products/{product}/delete-image', [AdminProductController::class, 'destroyImage'])->name("admin.product.destroyImage");
+
+});
+
+require __DIR__.'/auth.php';
+
